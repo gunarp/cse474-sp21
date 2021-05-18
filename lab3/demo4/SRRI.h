fdbef2220ba4eb53103af5d8ac0281994693ce10
Peter Gunarso <gunarp@cs.washington.edu>
2021-05-17 21:56:33 -0700

    demo 4 first commit

---
+++ lab3/demo4/SRRI.h
@@ -0,0 +1,62 @@
+#define BIT0 1<<0
+#define BIT1 1<<1
+#define BIT2 1<<2
+#define BIT3 1<<3
+
+#define TIMER_REG_A TCCR4A
+#define TIMER_REG_B TCCR4B
+#define TIMER_COUNTER TCNT4H
+#define PRESCALER OCR4A
+#define LED_DDR DDRL
+#define LED_PORT PORTL
+#define SPEAKER_DDR DDRH
+#define SPEAKER_PORT PORTH
+
+#define FLASH_DURATION 250
+#define NFLASH 3
+#define PLAY_DURATION 200
+#define ADURATION 2000
+#define CDURATION 10000
+#define PAUSE_DURATION 4000
+#define PICKUP_TIME (NFREQ * PLAY_DURATION) + PAUSE_DURATION
+
+#define note_c3 130
+#define note_c4 261
+#define note_d4 293
+#define note_e4 329
+#define note_g3 196
+#define note_R 0
+
+#define NFREQ 13
+int melody[] = { note_d4, note_R, note_e4, note_R, note_c4, note_R, note_c3,
+                 note_R, note_g3, note_g3, note_g3, note_g3, note_R };
+
+byte seven_seg_digits[10][7] = { { 1,1,1,1,1,1,0 },  // = 0
+                                 { 0,1,1,0,0,0,0 },  // = 1
+                                 { 1,1,0,1,1,0,1 },  // = 2
+                                 { 1,1,1,1,0,0,1 },  // = 3
+                                 { 0,1,1,0,0,1,1 },  // = 4
+                                 { 1,0,1,1,0,1,1 },  // = 5
+                                 { 1,0,1,1,1,1,1 },  // = 6
+                                 { 1,1,1,0,0,0,0 },  // = 7
+                                 { 1,1,1,1,1,1,1 },  // = 8
+                                 { 1,1,1,0,0,1,1 }   // = 9
+                                 };
+
+#define NTASKS 10
+
+// possible states for our tasks to be in
+enum state {READY, RUNNING, SLEEPING};
+
+enum flagState {PENDING, DONE};
+
+void sleep_474(long t);
+void schedule_sync();
+void setOC4AFreq(uint32_t freq);
+void task1();
+void task2();
+void task3();
+void convert(int * digits, int val);
+void interruptSetup();
+void speakerSetup();
+void ledSetup();
